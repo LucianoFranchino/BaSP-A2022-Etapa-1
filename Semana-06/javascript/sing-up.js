4abb6d57167e8ab8ee1d4f2bcaa763f582c13bea
Luciano Franchino <luciano_franchino@hotmail.com>
2022-09-27 19:31:24 -0300

    BaSP-W06-2022: Last corrections in sing-up js

--- Semana-06/javascript/sing-up.js
+++ Semana-06/javascript/sing-up.js
@@ -30,33 +30,34 @@ window.onload = function(){
     var errorWarning = document.createElement('p');
     errorWarning.classList.add("mensage-error");
     var errorAlert = [];
-    var error;
+    var errorEmail;
 
     emailInput.onblur = function(){
         if(emailInput.value == ''){
             emailInput.classList.add('red-border');
             errorWarning.textContent = 'Insert Email';
             emailInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Email error';
-            errorAlert.push(error);
+            errorEmail = 'Email error';
+            errorAlert.push(errorEmail);
         }else if(!emailExpression.test(emailInput.value)){
             emailInput.classList.add('red-border');
             errorWarning.textContent = 'Insert a valid Email';
             emailInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Email error';
-            errorAlert.push(error);
+            errorEmail = 'Email error';
+            errorAlert.push(errorEmail);
         }else{
             emailInput.classList.add('green-border');
         }
     }
     emailInput.onfocus = function(){
         emailInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorEmail);
         emailInput.classList.remove('red-border');
         emailInput.classList.remove('green-border');
         errorWarning.remove();
     }
 
+    var errorName;
     nameInput.onblur = function(){
         var hasLetter = 0;
         for(var i=0; i < nameInput.value.length; i++ ){
@@ -68,32 +69,33 @@ window.onload = function(){
             nameInput.classList.add('red-border');
             errorWarning.textContent = 'Insert a name';
             nameInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Name error';
-            errorAlert.push(error);
+            errorName = 'Name error';
+            errorAlert.push(errorName);
         }else if(nameInput.value.length < 3){
             nameInput.classList.add('red-border');
             errorWarning.textContent = 'Enter a name of more than 3 letters';
             nameInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Name error';
-            errorAlert.push(error);
+            errorName = 'Name error';
+            errorAlert.push(errorName);
         }else if(hasLetter === 0 ){
             nameInput.classList.add('red-border');
             errorWarning.textContent = 'Name can not contain numbers';
             nameInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Name error';
-            errorAlert.push(error);
+            errorName = 'Name error';
+            errorAlert.push(errorName);
         }else{
             nameInput.classList.add('green-border');
         }
     }
     nameInput.onfocus = function(){
         nameInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorName);
         nameInput.classList.remove('red-border');
         nameInput.classList.remove('green-border');
         errorWarning.remove();
     }
 
+    var errorLastName;
     lastNameInput.onblur = function(){
         var hasLetter = 0;
         for(var i=0; i < lastNameInput.value.length; i++ ){
@@ -105,32 +107,33 @@ window.onload = function(){
             lastNameInput.classList.add('red-border');
             errorWarning.textContent = 'Insert a Last Name';
             lastNameInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Last Name error';
-            errorAlert.push(error);
+            errorLastName = 'Last Name error';
+            errorAlert.push(errorLastName);
         }else if(lastNameInput.value.length < 3){
             lastNameInput.classList.add('red-border');
             errorWarning.textContent = 'Enter a Last Name of more than 3 letters';
             lastNameInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Last Name error';
-            errorAlert.push(error);
+            errorLastName = 'Last Name error';
+            errorAlert.push(errorLastName);
         }else if(hasLetter === 0 ){
             lastNameInput.classList.add('red-border');
             errorWarning.textContent = 'Last Name can not contain numbers';
             lastNameInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Last Name error';
-            errorAlert.push(error);
+            errorLastName = 'Last Name error';
+            errorAlert.push(errorLastName);
         }else{
             lastNameInput.classList.add('green-border');
         }
     }
     lastNameInput.onfocus = function(){
         lastNameInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorLastName);
         lastNameInput.classList.remove('red-border');
         lastNameInput.classList.remove('green-border');
         errorWarning.remove();
     }
 
+    var errorDni;
     documentInput.onblur = function(){
         var hasLetter = 0;
         for(var i=0; i < documentInput.value.length; i++ ){
@@ -142,51 +145,53 @@ window.onload = function(){
             documentInput.classList.add('red-border');
             errorWarning.textContent = 'Insert DNI';
             documentInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Document error';
-            errorAlert.push(error);
+            errorDni = 'Document error';
+            errorAlert.push(errorDni);
         }else if(documentInput.value.length < 7){
             documentInput.classList.add('red-border');
             errorWarning.textContent = 'DNI must have more than 7 numbers';
             documentInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Document error';
-            errorAlert.push(error);
+            errorDni = 'Document error';
+            errorAlert.push(errorDni);
         }else if(hasLetter === 0 ){
             documentInput.classList.add('red-border');
             errorWarning.textContent = 'DNI can not contain letters';
             documentInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Document error';
-            errorAlert.push(error);
+            errorDni = 'Document error';
+            errorAlert.push(errorDni);
         }else{
             documentInput.classList.add('green-border');
         }
     }
     documentInput.onfocus = function(){
         documentInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorDni);
         documentInput.classList.remove('red-border');
         documentInput.classList.remove('green-border');
         errorWarning.remove();
     }
 
+    var errorDate;
     dateInput.onblur = function(){
         if(dateInput.value == ''){
             dateInput.classList.add('red-border');
             errorWarning.textContent = 'Insert date';
             dateInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Date error';
-            errorAlert.push(error);
+            errorDate = 'Date error';
+            errorAlert.push(errorDate);
         }else{
             documentInput.classList.add('green-border');
         }
     }
     dateInput.onfocus = function(){
         dateInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorDate);
         dateInput.classList.remove('red-border');
         dateInput.classList.remove('green-border');
         errorWarning.remove();
     }
 
+    var errorPhone;
     phoneNumberInput.onblur = function(){
         var hasLetter = 0;
         for(var i=0; i < phoneNumberInput.value.length; i++ ){
@@ -198,32 +203,33 @@ window.onload = function(){
             phoneNumberInput.classList.add('red-border');
             errorWarning.textContent = 'Insert phone number';
             phoneNumberInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Phone Number error';
-            errorAlert.push(error);
+            errorPhone = 'Phone Number error';
+            errorAlert.push(errorPhone);
         }else if(phoneNumberInput.value.length < 10){
             phoneNumberInput.classList.add('red-border');
             errorWarning.textContent = 'Phone must have more than 10 numbers';
             phoneNumberInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Phone Number error';
-            errorAlert.push(error);
+            errorPhone = 'Phone Number error';
+            errorAlert.push(errorPhone);
         }else if(hasLetter === 0 ){
             phoneNumberInput.classList.add('red-border');
             errorWarning.textContent = 'Phone number can not contain letters';
             phoneNumberInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Phone Number error';
-            errorAlert.push(error);
+            errorPhone = 'Phone Number error';
+            errorAlert.push(errorPhone);
         }else{
             phoneNumberInput.classList.add('green-border');
         }
     }
     phoneNumberInput.onfocus = function(){
         phoneNumberInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorPhone);
         phoneNumberInput.classList.remove('red-border');
         phoneNumberInput.classList.remove('green-border');
         errorWarning.remove();
     }
 
+    var errorLocation;
     locationInput.onblur = function(){
         var hasNumber = 0;
         var hasLetter = 0;
@@ -238,26 +244,27 @@ window.onload = function(){
             locationInput.classList.add('red-border');
             errorWarning.textContent = 'Insert location';
             locationInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Location error';
-            errorAlert.push(error);
+            errorLocation = 'Location error';
+            errorAlert.push(errorLocation);
         }else if(hasLetter === 0 || hasNumber === 0 && locationInput.value.length < 3 ){
             locationInput.classList.add('red-border');
             errorWarning.textContent = 'Location most contain more of 3 characters';
             locationInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Location error';
-            errorAlert.push(error);
+            errorLocation = 'Location error';
+            errorAlert.push(errorLocation);
         }else{
             locationInput.classList.add('green-border');
         }
     }
     locationInput.onfocus = function(){
         locationInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorLocation);
         locationInput.classList.remove('red-border');
         locationInput.classList.remove('green-border');
         errorWarning.remove();
     }
 
+    var errorPostalCode;
     postalCodeInput.onblur = function(){
         var hasLetter = 0;
         for(var i=0; i < postalCodeInput.value.length; i++ ){
@@ -269,32 +276,33 @@ window.onload = function(){
             postalCodeInput.classList.add('red-border');
             errorWarning.textContent = 'Insert Postal Code';
             postalCodeInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Postal Code error';
-            errorAlert.push(error);
+            errorPostalCode = 'Postal Code error';
+            errorAlert.push(errorPostalCode);
         }else if(postalCodeInput.value.length < 4 || postalCodeInput.value.length > 5 ){
             postalCodeInput.classList.add('red-border');
             errorWarning.textContent = 'Postal Code must have between 4 and 5 numbers';
             postalCodeInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Postal Code error';
-            errorAlert.push(error);
+            errorPostalCode = 'Postal Code error';
+            errorAlert.push(errorPostalCode);
         }else if(hasLetter === 0 ){
             postalCodeInput.classList.add('red-border');
             errorWarning.textContent = 'Postal code can not contain letters';
             postalCodeInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Postal Code error';
-            errorAlert.push(error);
+            errorPostalCode = 'Postal Code error';
+            errorAlert.push(errorPostalCode);
         }else{
             postalCodeInput.classList.add('green-border');
         }
     }
     postalCodeInput.onfocus = function(){
         postalCodeInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorPostalCode);
         postalCodeInput.classList.remove('red-border');
         postalCodeInput.classList.remove('green-border');
         errorWarning.remove();
     }
 
+    var errorAdress;
     adressInput.onblur = function(){
         var hasNumber = 0;
         var hasLetter = 0;
@@ -308,32 +316,33 @@ window.onload = function(){
             adressInput.classList.add('red-border');
             errorWarning.textContent = 'Insert adress';
             adressInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Adress error';
-            errorAlert.push(error);
+            errorAdress = 'Adress error';
+            errorAlert.push(errorAdress);
         }else if(!adressInput.value.includes(' ')){
             adressInput.classList.add('red-border');
             errorWarning.textContent = 'Must include space';
             adressInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Adress error';
-            errorAlert.push(error);
+            errorAdress = 'Adress error';
+            errorAlert.push(errorAdress);
         }else if(hasLetter === 0 || hasNumber === 0 && adressInput.value.length < 5 ){
             adressInput.classList.add('red-border');
             errorWarning.textContent = 'Adress most contain more of 5 characters';
             adressInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Adress error';
-            errorAlert.push(error);
+            errorAdress = 'Adress error';
+            errorAlert.push(errorAdress);
         }else{
             adressInput.classList.add('green-border');
         }
     }
     adressInput.onfocus = function(){
         adressInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorAdress);
         adressInput.classList.remove('red-border');
         adressInput.classList.remove('green-border');
         errorWarning.remove();
     }
 
+    var errorPassword;
     passwordInput.onblur = function(){
         var hasNumber = 0;
         var hasLetter = 0;
@@ -348,47 +357,47 @@ window.onload = function(){
             passwordInput.classList.add('red-border');
             errorWarning.textContent = 'Insert password';
             passwordInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Password error';
-            errorAlert.push(error);
+            errorPassword = 'Password error';
+            errorAlert.push(errorPassword);
         }else if(hasLetter === 0 || hasNumber === 0 ){
             passwordInput.classList.add('red-border');
             errorWarning.textContent = 'Password most contain more of 4 characters, letters and numbers';
             passwordInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Password error';
-            errorAlert.push(error);
+            errorPassword = 'Password error';
+            errorAlert.push(errorPassword);
         }else{
             passwordInput.classList.add('green-border');
         }
     }
     passwordInput.onfocus = function(){
         passwordInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorPassword);
         passwordInput.classList.remove('red-border');
         passwordInput.classList.remove('green-border');
         errorWarning.remove();
     }
-
     
+    var errorRepeatPassword;
     repeatPasswordInput.onblur = function(){
         if(repeatPasswordInput.value !== passwordInput.value){
             repeatPasswordInput.classList.add('red-border');
             errorWarning.textContent = 'Passwords do not match';
             repeatPasswordInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Repeat Password error';
-            errorAlert.push(error);
+            errorRepeatPassword = 'Repeat Password error';
+            errorAlert.push(errorRepeatPassword);
         }else if(repeatPasswordInput.value == ''){
             repeatPasswordInput.classList.add('red-border');
             errorWarning.textContent = 'Insert password';
             repeatPasswordInput.insertAdjacentElement('afterend', errorWarning);
-            error = 'Repeat Password error';
-            errorAlert.push(error);
+            errorRepeatPassword = 'Repeat Password error';
+            errorAlert.push(errorRepeatPassword);
         }else{
             repeatPasswordInput.classList.add('green-border');
         }
     }
     repeatPasswordInput.onfocus = function(){
         repeatPasswordInput.value = '';
-        errorAlert = errorSelector(errorAlert,error);
+        errorAlert = errorSelector(errorAlert,errorRepeatPassword);
         repeatPasswordInput.classList.remove('red-border');
         repeatPasswordInput.classList.remove('green-border');
         errorWarning.remove();
